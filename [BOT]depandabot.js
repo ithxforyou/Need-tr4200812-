17de57b53d1b56b902043a05624a58bf1d239310
Sarut Limpajeerawong| |github.tr4200812.github.com <179701381+tr4200812@users.noreply.github.com>
2025-05-19 00:39:40 -0400

    Create [BOT]depandabot.js
    Signed-off-by: Sarut Limpajeerawong| |github.tr4200812.github.com <179701381+tr4200812@users.noreply.github.com>

---
+++ [BOT]depandabot.js
@@ -0,0 +1,17 @@
+## "Refresh'
+
+Dependabot
+.github/dependabot.yml
+Dependabot / .github/dependabot.yml
+failed last week in 0s
+Your .github/dependabot.yml contained invalid details
+Dependabot encountered the following error when parsing your .github/dependabot.yml:
+
+The property '#/updates/0/package-ecosystem' value "js" did not match one of the following values: npm, bundler, composer, devcontainers, dotnet-sdk, maven, mix, cargo, gradle, nuget, gomod, docker, docker-compose, elm, gitsubmodule, github-actions, pip, terraform, pub, swift, bun, uv, helm
+Please update the config file to conform with Dependabot's specification.
+
+Details
+For more info on the config file format, see the config file documentation
+
+View more details on [BOT]dependabot.js
+exit-00
